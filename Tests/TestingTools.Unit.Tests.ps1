ec20b1a6ff53a554a1cadd78e3ff2f360a27792c
Mitchell Gill <mgill@opensequence.net>
2019-07-11 09:27:25 +1000

    tests

--- Tests/TestingTools.Unit.Tests.ps1
+++ Tests/TestingTools.Unit.Tests.ps1
@@ -49,23 +49,56 @@ Describe Copy-FunctionsFromScript {
 
         #Assert
         #Add Parameters and call function
+        $testPath = Convert-Path TestDrive:
         $Parameters = @{
             path    = "$($ScriptPath.FullName)\Resources\FakeScript-2Functions.ps1"
-            NewPath = "TestDrive:\FakeScript-Just2Functions.ps1"
+            NewPath = "$($testPath)\FakeScript-Just2Functions.ps1"
             Verbose = $true
         }
         #Copy functions from List File
         Copy-FunctionsFromScript @Parameters
 
         #Read in New file for comparison
-        [System.Collections.Generic.List[string]]$newFile = [System.IO.File]::ReadAllLines($Parameters.path)
+        [System.Collections.Generic.List[string]]$newFile = [System.IO.File]::ReadAllLines($Parameters.NewPath)
 
         It "Dot Sourcing the File Should not Throw" {
             { . "TestDrive:\FakeScript-Just2Functions.ps1" } | Should -Not -Throw
         }
 
-        It "Should Contain 2 Functions" {
+        It "Should Contain 2 Standard PowerShell Functions" {
             $newFile.where( { $_ -Like "*function *" }).Count | Should -Be 2
+        }
+
+    }
+
+    Context "Script Containing 2 Standard functions & 1 Class with Methods" {
+
+        #Mocks
+
+        #Assert
+        #Add Parameters and call function
+        $testPath = Convert-Path TestDrive:
+        $Parameters = @{
+            path    = "$($ScriptPath.FullName)\Resources\FakeScript-2FunctionsWithClassMethod.ps1"
+            NewPath = "$($testPath)\FakeScript-Just2Functions.ps1"
+            Verbose = $true
+        }
+        #Copy functions from List File
+        Copy-FunctionsFromScript @Parameters
+
+        #Read in New file for comparison
+        [System.Collections.Generic.List[string]]$newFile = [System.IO.File]::ReadAllLines($Parameters.NewPath)
+
+        It "Dot Sourcing the File Should not Throw" {
+            { . "TestDrive:\FakeScript-Just2Functions.ps1" } | Should -Not -Throw
+        }
+
+        It "Should Contain 2 Standard PowerShell Functions" {
+            $newFile.where( { $_ -Like "*function *" }).Count | Should -Be 2
+        }
+
+        It "Should NOT Contain A Seperated Class Method" {
+            $newFile.where( { $_ -Like "SoundHorn ()*" }).Count | Should -Be 0
         }
 
     }
