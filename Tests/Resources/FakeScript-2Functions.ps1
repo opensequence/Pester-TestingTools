4a27e885d06c7236c5838ce95dd54bd8734aa773
Mitchell Gill <mgill@opensequence.net>
2019-07-11 08:58:09 +1000

    adding tests

---
+++ Tests/Resources/FakeScript-2Functions.ps1
@@ -0,0 +1,136 @@
+<#
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+
+
+.NOTES
+AUTHOR  : Mitchell Gill
+CREATED : 12/02/2019
+VERSION : 12/02/2019
+			1.0 - Initial Revision
+
+.INPUTS
+None. This script does not accept input parameters or pipelines
+
+.OUTPUTS
+None. This script does not output parameters
+
+.EXAMPLE
+
+
+#>
+#====================================================================================================
+#                                             Parameters
+#====================================================================================================
+#region Parameters
+
+[CmdletBinding()]
+Param(
+
+    [Parameter(Mandatory = $false)]
+    [string]$InstanceID,
+
+    [Parameter(Mandatory = $false)]
+    [string]$RegionID,
+
+    [Parameter(Mandatory = $false)]
+    [string[]]$AMIID
+
+)
+
+#Set the error action preference to stop to handle errors as they occur
+$ErrorActionPreference = "Stop"
+
+#Override the verbose preference
+$VerbosePreference = "SilentlyContinue"
+
+#endregion Parameters
+
+#Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.428.0'}
+#====================================================================================================
+#                                             Functions
+#====================================================================================================
+#region Functions
+
+#region Find-MeInstance
+
+function Find-MeInstance {
+    [CmdletBinding()]
+    Param (
+        [parameter(Mandatory = $True, Position = 1)]
+        [string]
+        $InstanceID,
+        [parameter(Mandatory = $True, Position = 2)]
+        [string]
+        $Region,
+        [parameter(Mandatory = $True, Position = 3)]
+        [string[]]
+        $AMIID
+    )
+    # retrieve EC2Instance Details
+    try {
+        Write-Verbose "START: Retrieving EC2 Details for Instance: $($InstanceID)"
+        $EC2InstanceDetail = Get-EC2Instance -InstanceID $InstanceID -Region $Region
+        Write-Verbose "SUCCESS: Retrieving EC2 Details for Instance: $($InstanceID)"
+    } catch {
+        Throw "ERROR: Retrieving EC2 Details for Instance: $($InstanceID) ErrorMessage: $($_.Exception.Message)"
+    }
+
+    #Compare AMI ID wth provided ID
+    If ($AMIID -contains $($EC2InstanceDetail.Instances.ImageId)) {
+        Write-Verbose "$($EC2InstanceDetail.Instances.ImageId) Matches one of Provided AMIID: $($AMIID)"
+        return $EC2InstanceDetail
+    } else {
+        Write-Verbose "$($EC2InstanceDetail.Instances.ImageId) does NOT Match one of Provided AMIID: $($AMIID)"
+        return $null
+    }
+
+}
+#endregion Find-MeInstance
+
+Function Imafunction () {
+
+}
+
+
+#endregion Functions
+
+#====================================================================================================
+#                                          Initialize Code
+#====================================================================================================
+#region Initialize Code
+If (-not $InstanceID) {
+    Exit
+}
+
+#endregion Initialize Code
+#====================================================================================================
+#                                             Main Code
+#====================================================================================================
+#region Main Code
+
+#retrieve the AMI ID of the Instance
+try {
+    Write-Output "START: Retrieving EC2 Details for Instance: $($InstanceID)"
+    #TODO-NOW Can you see the mistake I've made here?
+    $MatchingEC2Instance = Find-MeInstance -InstanceID "$($InstanceID)" -Region $Region -AMIID $AMIID #it's here
+    Write-Output "SUCCESS: Retrieving EC2 Details for Instance: $($InstanceID)"
+} catch {
+    Write-Error "ERROR: Retrieving EC2 Details for Instance: $($InstanceID) ErrorMessage: $($_.Exception.Message)"
+    Exit
+}
+
+If (-not $MatchingEC2Instance) {
+    Write-Output "Instance does not match that AMI!"
+    #Notify
+    Publish-SNSMessage
+} else {
+    Write-Output "SUCCESS: Tagging Instance: $($InstanceID)"
+    #Tag
+    New-EC2Tag
+}
+
+#endregion Main Code
---
+++ Tests/TestingTools.Unit.Tests.ps1
@@ -0,0 +1,73 @@
+Describe Remove-FunctionsFromScript {
+    $ScriptPath = (get-item (Split-Path $script:MyInvocation.MyCommand.Path))
+
+    #Dotsource in the Powershell Script
+    . "$($ScriptPath.parent.FullName)\TestingTools.ps1"
+
+    Context "Script Containing 2 Standard functions" {
+
+        #Mocks
+
+        #Assert
+        #Add Parameters and call function
+        $Parameters = @{
+            path    = "$($ScriptPath.FullName)\Resources\FakeScript-2Functions.ps1"
+            Verbose = $true
+        }
+        #Read in Original file for comparison
+        [System.Collections.Generic.List[string]]$original = [System.IO.File]::ReadAllLines($Parameters.path)
+
+        #Strip functions from List File
+        [System.Collections.Generic.List[String]]$result = Remove-FunctionsFromScript @Parameters
+
+        It "Result Should Not be Null" {
+            $result | Should -Not -BeNullOrEmpty
+        }
+
+        It "Should Not Contain any Functions" {
+            $result.where( { $_ -Like "*function *" }) | Should -Be $null
+        }
+
+        It "Result Should Be Shorter than Original Length" {
+            $result.Count | Should -BeLessThan $original.Count
+        }
+
+
+    }
+
+}
+
+Describe Copy-FunctionsFromScript {
+    $ScriptPath = (get-item (Split-Path $script:MyInvocation.MyCommand.Path))
+
+    #Dotsource in the Powershell Script
+    . "$($ScriptPath.parent.FullName)\TestingTools.ps1"
+
+    Context "Script Containing 2 Standard functions" {
+
+        #Mocks
+
+        #Assert
+        #Add Parameters and call function
+        $Parameters = @{
+            path    = "$($ScriptPath.FullName)\Resources\FakeScript-2Functions.ps1"
+            NewPath = "TestDrive:\FakeScript-Just2Functions.ps1"
+            Verbose = $true
+        }
+        #Copy functions from List File
+        Copy-FunctionsFromScript @Parameters
+
+        #Read in New file for comparison
+        [System.Collections.Generic.List[string]]$newFile = [System.IO.File]::ReadAllLines($Parameters.path)
+
+        It "Dot Sourcing the File Should not Throw" {
+            { . "TestDrive:\FakeScript-Just2Functions.ps1" } | Should -Not -Throw
+        }
+
+        It "Should Contain 2 Functions" {
+            $newFile.where( { $_ -Like "*function *" }).Count | Should -Be 2
+        }
+
+    }
+
+}
